b81d859008664f3a3dd3aa4a7e2198a443a6ee1c
Yojhan Garcia <ygarcia-2022194@kinal.edu.gt>
2024-01-24 11:09:51 -0600

    Se arreglo la galeria de fotos en la pagina principal

--- assets/scripts/principal.js
+++ assets/scripts/principal.js
@@ -9,39 +9,5 @@ $(function(){
     });
 });
 
-let currentIndex = 0;
 
-document.querySelector('.prev-button').addEventListener('click', ()=>{
-    navigate(-1);
-})
-
-document.querySelector('.next-button').addEventListener('click', () =>{
-    navigate(1);
-})
-
-function navigate(){
-    const galleryContainer = document.querySelector('.gallery-container');
-    const totalImages = document.querySelectorAll('.gallery-item').length;
-
-    currentIndex=(currentIndex + direction + totalImages) % totalImages;
-
-    const offset = -currentIndex * 100;
-
-    galleryContainer.style.transform = `translateX(${offset}%)`;
-}
-
-let autoplayInterval = null;
-
-function startAutoplay(interval){
-    stopAutoplay();
-    autoplayInterval = setInterval(()=>{
-        navigate(1);
-    }, interval);
-}
-
-function stopAutoplay(){
-    clearInterval(autoplayInterval);
-}
-
-startAutoplay(3000);
 

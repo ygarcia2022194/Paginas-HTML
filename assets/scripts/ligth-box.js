69b1622bc26ab2eb6be8d30a70a7cd725f2f7672
Yojhan Garcia <ygarcia-2022194@kinal.edu.gt>
2024-01-26 20:55:39 -0600

    Se realizo el javaScript del menu de la pagina de kinal

---
+++ assets/scripts/ligth-box.js
@@ -0,0 +1,8 @@
+const imagenes = document.querySelectorAll('.img-galeria');
+const imagenesLigth = document.querySelector('.agregar-imagen');
+const contenedorLigth = document.querySelector('.imagen-light');
+
+console.log(imagenes)
+console.log(imagenesLigth)
+console.log(contenedorLigth)
+
